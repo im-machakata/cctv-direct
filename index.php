bcd71a4cb5382182153f98d5642248559f86918c
Isaac Machakata <isaac@caasi.co.zw>
2023-06-26 17:33:27 +0200

    refactor: fetch from server by default

--- index.php
+++ index.php
@@ -6,17 +6,22 @@ include __DIR__ . "/vendor/autoload.php";
 header('Content-Type: application/json');
 
 $results  = array();
-$host = $_SERVER['HTTP_HOST'];
 $scrapper = new Scrapper();
-$scrapper->scrape(sprintf('http://%s/bin/dahua-analog-hd-cctv-cameras.htm',$host));
+
+// ? use this on when not connected to the internet
+// $host     = sprintf('http://%s/bin/dahua-analog-hd-cctv-cameras.htm', $_SERVER['HTTP_HOST']);
+
+// ? scrap from the server directly
+$host = sprintf('https://cctv-direct.co.za/collections/dahua-analog-hd-cctv-cameras.html');
+$scrapper->scrape($host);
 
 // ? localhost demo scrapping 
 $collection = $scrapper->collection;
 
-if(!$collection){
-    die("Failed to connect to ".$host);
+if (!$collection) {
+    die("Failed to connect to " . $host);
 }
-$results    = array(
+$results = array(
     'title' => $collection->getTitle(),
     'category_slug' => $collection->getCategorySlug(),
     'products' => $collection->getProducts(),
@@ -25,15 +30,19 @@ $results    = array(
 );
 
 // ? scrapping all collections from official website
-// $collections = $scrapper->collection->getCollections();
+$collections = $scrapper->collection->getCollections();
 
-// foreach ($collections as $collectionUrl) {
-//     $scrapper->scrape($collectionUrl);
-//     $results[$scrapper->collection->getCategorySlug()] = array(
-//         'title' => $scrapper->collection->getTitle(),
-//         'products' => $scrapper->collection->getProducts()
-//     );
-// }
+foreach ($collections as $collectionUrl) {
+    $scrapper->scrape($collectionUrl);
+
+    if (!$scrapper->collection) {
+        continue;
+    }
+    $results[$scrapper->collection->getCategorySlug()] = array(
+        'title' => $scrapper->collection->getTitle(),
+        'products' => $scrapper->collection->getProducts()
+    );
+}
 
 // ? print results
 echo json_encode($results, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
