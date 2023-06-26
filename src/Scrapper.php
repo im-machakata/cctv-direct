188ce1980c9700202c2daa2549256a37b100b651
Isaac Machakata <isaac@caasi.co.zw>
2023-06-26 17:32:36 +0200

    feat: use custom fetch function

--- src/Scrapper.php
+++ src/Scrapper.php
@@ -1,7 +1,7 @@
 <?php
 namespace CctvDirect;
 
-use simplehtmldom\HtmlWeb;
+use simplehtmldom\HtmlDocument;
 
 /**
  * Scrapes data from cctv-direct.co.za
@@ -12,10 +12,6 @@ use simplehtmldom\HtmlWeb;
  */
 class Scrapper {
     /**
-     * @var HtmlWeb
-     */
-    private $scrapper;
-    /**
      * @var Collection|null
      */
     public $collection;
@@ -25,7 +21,6 @@ class Scrapper {
     public $wasScrapped;
 
     public function __construct() {
-        $this->scrapper   = new HtmlWeb();
         $this->collection = new Collection();
     }
 
@@ -35,9 +30,57 @@ class Scrapper {
      * @param string $url
      */
     public function scrape($url) {
-        $scrapped         = $this->scrapper->load($url);
+        $html             = $this->get_url($url);
+        $scrapped         = isset($html) ? new HtmlDocument($html) : null;
         $this->collection = !$scrapped ? null : $this->collection->source($scrapped, $url);
+
+        // free memory
         unset($scrapped);
+
         return ($this->wasScrapped = ($this->collection !== null));
+    }
+    /**
+     * Fetches the html contents of the url
+     * @param string $url
+     * @param int $javascript_loop 
+     * @param int $timeout
+     * @return string
+     */
+    private function get_url($url, $javascript_loop = 0, $timeout = 5) {
+        $url = str_replace("&amp;", "&", urldecode(trim($url)));
+
+        $cookie = tempnam("/tmp", "CURLCOOKIE");
+        $ch     = curl_init();
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; rv:1.7.3) Gecko/20041001 Firefox/0.10.1");
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_ENCODING, "");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); # required for https urls
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+        curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+        $content  = curl_exec($ch);
+        $response = curl_getinfo($ch);
+        curl_close($ch);
+
+        if ($response['http_code'] == 301 || $response['http_code'] == 302) {
+            ini_set("user_agent", "Mozilla/5.0 (Windows; U; Windows NT 5.1; rv:1.7.3) Gecko/20041001 Firefox/0.10.1");
+
+            if ($headers = get_headers($response['url'])) {
+                foreach ($headers as $value) {
+                    if (substr(strtolower($value), 0, 9) == "location:")
+                        return $this->get_url(trim(substr($value, 9, strlen($value))));
+                }
+            }
+        }
+
+        if ((preg_match("/>[[:space:]]+window\.location\.replace\('(.*)'\)/i", $content, $value) || preg_match("/>[[:space:]]+window\.location\=\"(.*)\"/i", $content, $value)) && $javascript_loop < 5) {
+            return $this->get_url($value[1], $javascript_loop + 1);
+        } else {
+            return $content;
+        }
     }
 }
